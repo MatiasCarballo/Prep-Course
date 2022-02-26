eb2bf1576c87ab191b575f597e143831a6cbccc7
MatiasCarballo <matiascarballo03@hotmail.com>
2022-02-26 10:53:00 -0300

    EXtra Terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+    var nuevoArray = [];
+    for(var i in objeto){
+      nuevoArray.push([i, objeto[i]]);
+      }
+    return nuevoArray;
 }
 
 
@@ -18,6 +24,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  var objeto = {};
+  for(var i = 0; i < string.length; i++)
+    if(objeto.hasOwnProperty(string[i])){
+      objeto[string[i]] = objeto[string[i]] + 1;
+    }else {
+      objeto[string[i]] = 1;
+    }
+    return objeto;
+
 }
 
 
@@ -26,6 +42,21 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+
+  var convertir = s.split("");
+  var mayus = [];
+  var minus = [];
+  for(var i = 0; i < convertir.length; i++){
+    if(convertir[i] === convertir[i].toUpperCase()){
+      mayus.push(convertir[i]);
+    }else {
+      minus.push(convertir[i]);
+    }
+  }
+  
+  return mayus.join("") + minus.join("");
+
+
 }
 
 
@@ -35,6 +66,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+  var convertir = str.split(" "); //separa por palabra y se guarda en covertir
+  var respuesta = [];
+  for(i = 0;i < convertir.length; i++){
+   respuesta.push(convertir[i].split("").reverse().join(""));  
+  }   //cada palabra es separada, se inverte cada elemento y se junta nuevamente en un string
+  return respuesta.join(" ");   //cada string invertido vuelve a juntarse en un solo string
+
+
 } 
 
 
@@ -43,6 +83,17 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+
+  
+  var num = numero.toString();
+
+  var numinvertido = num.split("").reverse().join("");
+  if(num === numinvertido){
+    return "Es capicua";
+  }else{
+    return "No es capicua";
+  }
+
 }
 
 
@@ -50,6 +101,18 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+  var sinABC = "";
+  for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] === "a" || cadena[i] === "b" || cadena[i] === "c"){
+      continue;
+    }else{
+      sinABC += cadena[i];
+    }
+  }
+  return sinABC;
+
+
 }
 
 
@@ -57,6 +120,20 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  var cambio = true;
+  while(cambio){
+    cambio = false;
+    for(i = 0; i < arr.length - 1; i++){
+      if(arr[i].length > arr[i + 1].length){
+        var aux = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = aux;
+        cambio = true;
+      }
+    }
+  }
+return arr;
 }
 
 
@@ -66,6 +143,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  var elemento = [];
+  for(i=0; i<arreglo1.length; i++){
+    for(j=0; j<arreglo2.length; j++){
+      if(arreglo1[i]=== arreglo2[j]){
+        elemento.push(arreglo1[i]); //tambien puso ser el el arreglo2, al final termina siendo lo mismo
+      }
+    }
+  }
+  return elemento;
 }
 
 
